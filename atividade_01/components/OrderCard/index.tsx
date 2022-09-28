caa8276a371ac672f70e1d6480cb9cf92195aff5
HigorBusenitz <105672186+HigorBusenitz@users.noreply.github.com>
2022-09-28 19:59:40 -0300

    atividade_01

---
+++ atividade_01/components/OrderCard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const OrderCard: React.FC = () => {
+  return (
+    <View>
+      <Text>ASD</Text>
+    </View>
+  );
+};
+
+export default OrderCard;
